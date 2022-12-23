eaac9ee096523c118524c831715b172ff26b3586
MrILL <ilua2403@ukr.net>
2022-12-24 01:53:39 +0200

    clean: unused file

--- srape.py
+++
@@ -1,7 +0,0 @@
-import cloudscraper
-
-site = "https://ranobes.com/chapters/solo-leveling-org/23557-tom-1-glava-1-ohotnik-ranga-e.html"
-
-scraper = cloudscraper.create_scraper()  # returns a CloudScraper instance
-# Or: scraper = cloudscraper.CloudScraper()  # CloudScraper inherits from requests.Session
-print(scraper.get(site).text)  # => "<!DOCTYPE html><html><head>..."
